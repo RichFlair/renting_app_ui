741c4dff290988d6001595acf5de7387585fc002
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-16 03:35:00 +0000

    adjusting padding

--- lib/ui/homepage/widgets/text_and_item_cards/text_and_item_cards.dart
+++ lib/ui/homepage/widgets/text_and_item_cards/text_and_item_cards.dart
@@ -6,42 +6,48 @@ class TextAndItemCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('Find the best to rent'),
-          SizedBox(
-            height: 90,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(8),
-                  height: double.maxFinite,
-                  width: 90,
-                  decoration: const BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(25),
-                    ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Text(
+              'Find the best to rent',
+              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                    color: Colors.white,
                   ),
-                  child: Center(
-                    child: const Text(
-                      'Item',
-                      style: TextStyle(
-                        color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            child: SizedBox(
+              height: 90,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.all(8),
+                    height: double.maxFinite,
+                    width: 90,
+                    decoration: const BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
                       ),
                     ),
-                  ),
-                );
-              },
-              itemCount: 5,
-              separatorBuilder: (context, index) {
-                return const SizedBox(
-                  width: 15,
-                );
-              },
+                    child: Center(
+                      child: Image.asset('assets/tent.png'),
+                    ),
+                  );
+                },
+                itemCount: 5,
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    width: 15,
+                  );
+                },
+              ),
             ),
           )
         ],
