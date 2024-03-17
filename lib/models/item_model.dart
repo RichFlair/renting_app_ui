d222b4c9b0d569186c2d04425a739e0f6ca562dc
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-17 01:21:53 +0000

    defining an item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  final String name;
+  final String image;
+  final Color backgroundColor;
+
+  Item({
+    required this.name,
+    required this.image,
+    required this.backgroundColor,
+  });
+}
