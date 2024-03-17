027cca2665adc6efa45d628da87de909f63422b4
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-17 01:41:46 +0000

    creating a dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+
+final List<Item> items = [
+  Item(
+    name: 'Sports',
+    image: 'assets/cycle.png',
+    backgroundColor: const Color(0xffB4F8BA),
+  ),
+  Item(
+    name: 'Treadmill',
+    image: 'assets/fitness.png',
+    backgroundColor: const Color(0xffD09DF7),
+  ),
+  Item(
+    name: 'Screwdriver',
+    image: 'assets/screwdriver.png',
+    backgroundColor: const Color(0xffFE94AA),
+  ),
+  Item(
+    name: 'Fridge',
+    image: 'assets/fridge.png',
+    backgroundColor: const Color(0xffFEC17A),
+  ),
+  Item(
+    name: 'Drill',
+    image: 'assets/drill.png',
+    backgroundColor: const Color.fromARGB(255, 148, 254, 229),
+  ),
+  Item(
+    name: 'Tablet',
+    image: 'assets/tablet.png',
+    backgroundColor: const Color(0xffD09DF7),
+  ),
+  Item(
+    name: 'Tent',
+    image: 'assets/tent.png',
+    backgroundColor: const Color(0xffB4F8BA),
+  ),
+  Item(
+    name: 'Utensils',
+    image: 'assets/utensil.png',
+    backgroundColor: const Color(0xffFEC17A),
+  ),
+];
