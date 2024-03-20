f4f992d3d94fa436822ca666d3c8ae92cbcd4ee1
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-20 01:20:23 +0000

    creating a product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final String image;
+  final double price;
+  final bool isFavorite;
+  final double rating;
+
+  Product({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.isFavorite,
+    required this.rating,
+  });
+}
