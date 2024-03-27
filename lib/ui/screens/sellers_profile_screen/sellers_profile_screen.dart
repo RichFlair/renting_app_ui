76fe1e77f8808a1bc0c7a0695939937370d69ddd
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-27 23:40:53 +0000

    adding bacground color

---
+++ lib/ui/screens/sellers_profile_screen/sellers_profile_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SellersProfileScreen extends StatelessWidget {
+  static const routName = 'sellers_profile_screen';
+  const SellersProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
