2c9e33848f687ee64c7e28a71d44ed8c7dbb8f24
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-28 03:31:32 +0000

    action buttons

--- lib/ui/screens/sellers_profile_screen/sellers_profile_screen.dart
+++ lib/ui/screens/sellers_profile_screen/sellers_profile_screen.dart
@@ -9,8 +9,15 @@ class SellersProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xff151617),
       appBar: AppBar(
+        centerTitle: true,
         foregroundColor: Colors.white,
         backgroundColor: const Color(0xff151617),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_horiz),
+          ),
+        ],
       ),
     );
   }
