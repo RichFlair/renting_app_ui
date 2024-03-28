7f35f93da6bbefba479b69e7918af04fd5d8fb93
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-28 18:08:25 +0000

    using a page.builder

--- lib/ui/screens/product_details_screen/widgets/prod_details_image_card.dart
+++ lib/ui/screens/product_details_screen/widgets/prod_details_image_card.dart
@@ -19,92 +19,102 @@ class ProdDetailsImageCard extends StatefulWidget {
 }
 
 class _ProdDetailsImageCardState extends State<ProdDetailsImageCard> {
+  int activePageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 250,
-      margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade200,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(20),
-        ),
-      ),
-      child: Stack(
-        children: [
-          Center(
-            child: Image(
-              image: AssetImage(
-                widget.imagePath,
+      child: PageView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 250,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 10,
-            left: 15,
-            child: Row(
+            child: Stack(
               children: [
-                Container(
-                  padding: const EdgeInsets.all(4),
-                  width: 100,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(15),
+                Center(
+                  child: Image(
+                    image: AssetImage(
+                      widget.imagePath,
                     ),
                   ),
-                  child: const Center(
-                    child: Row(
-                      children: [
-                        Icon(
-                          IconlyBold.location,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          '3.2 km',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 15,
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        width: 100,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(15),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  padding: const EdgeInsets.all(4),
-                  width: 100,
-                  height: 35,
-                  decoration: const BoxDecoration(
-                    color: Color(0xff2AB364),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Available',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                        child: const Center(
+                          child: Row(
+                            children: [
+                              Icon(
+                                IconlyBold.location,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                '3.2 km',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(width: 10),
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        width: 100,
+                        height: 35,
+                        decoration: const BoxDecoration(
+                          color: Color(0xff2AB364),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Available',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Rating(rating: widget.rating),
+                    ],
                   ),
-                ),
-                const SizedBox(width: 10),
-                Rating(rating: widget.rating),
+                )
               ],
             ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
